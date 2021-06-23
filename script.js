5497e73a1aab77f2e1eca197069418d1d777e5a5
Arthur Melo <arthurmelo1000@gmail.com>
2021-06-22 21:09:51 -0300

    create css

--- script.js
+++ script.js
@@ -1,57 +1,71 @@
 window.onload = () => {
-    class Maze {
+   
+  const canvas = document.getElementsByClassName("canvas");
+  const ctx = canvas.getContext('2d');
+  
+  class Backgroung {
+     constructor() {
+       this.posX = 0;
+       this.posY = 0;
 
-    }
+       const img = new Image();
+       img.src = 
+     }
+     
+   }
+  // class Maze {
+
+    // }
     
-    class Timer {
-        constructor() {
-          this.currentTime = 0;
-          this.intervalId = null;
-        }
+    // class Timer {
+    //     constructor() {
+    //       this.currentTime = 0;
+    //       this.intervalId = null;
+    //     }
       
-        start(callback) {
-          this.intervalId = setInterval(() => {
-            this.currentTime += 1
-          },1000)
-        }
+    //     start(callback) {
+    //       this.intervalId = setInterval(() => {
+    //         this.currentTime += 1
+    //       },1000)
+    //     }
       
-        getMinutes() {
-          return Math.floor(this.currentTime/60);
-        }
+    //     getMinutes() {
+    //       return Math.floor(this.currentTime/60);
+    //     }
       
-        getSeconds() {
-          return this.currentTime%60
-        }
+    //     getSeconds() {
+    //       return this.currentTime%60
+    //     }
       
-        computeTwoDigitNumber(value) {
-        // let minutes = this.getMinutes();
-        // let seconds = this.getSeconds();
-        if (value < 10) {
-          return '0' + value;
-        } else {
-          return value.toString();
-        }
-        }
-        // if (seconds < 10) {
-        //   seconds = '0' + this.getSeconds();
-        // } else if (seconds >= 10) {
-        //   seconds = this.getSeconds();
-        // }
-        // let minutes = (this.getMinutes() < 10 ? '0': '') + this.getMinutes(); 
-        // let seconds = (this.getSeconds() < 10 ? '0': '') + this.getSeconds();
-        // value = minutes || seconds
-        stop() {
-          clearInterval(this.intervalId)
-        }
+    //     computeTwoDigitNumber(value) {
+    //     // let minutes = this.getMinutes();
+    //     // let seconds = this.getSeconds();
+    //     if (value < 10) {
+    //       return '0' + value;
+    //     } else {
+    //       return value.toString();
+    //     }
+    //     }
+    //     // if (seconds < 10) {
+    //     //   seconds = '0' + this.getSeconds();
+    //     // } else if (seconds >= 10) {
+    //     //   seconds = this.getSeconds();
+    //     // }
+    //     // let minutes = (this.getMinutes() < 10 ? '0': '') + this.getMinutes(); 
+    //     // let seconds = (this.getSeconds() < 10 ? '0': '') + this.getSeconds();
+    //     // value = minutes || seconds
+    //     stop() {
+    //       clearInterval(this.intervalId)
+    //     }
       
-        reset() {
-          this.currentTime = 0;
-        }
+    //     reset() {
+    //       this.currentTime = 0;
+    //     }
       
-        split() {
-          let visualSplit = this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds())
-          return visualSplit
-        }
-    }
+    //     split() {
+    //       let visualSplit = this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds())
+    //       return visualSplit
+    //     }
+    // }
       
 }
