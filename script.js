ac7ca6954fa7fc7a05c25ba7dd330547d64392da
Arthur Melo <arthurmelo1000@gmail.com>
2021-07-01 22:01:32 -0300

    Lógica de Perder e Ganhar

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ window.onload = () => {
   
   const canvas = document.getElementById("canvas");
   const start = document.getElementById("start-game");
+  const timer = document.getElementById("timer");
+  const scoreElement = document.getElementById("score");
   const ctx = canvas.getContext('2d');
+  
 
   start.addEventListener('click', updateCanvas)
+  // timer.addEventListener('click', updateCanvas)
 
   document.addEventListener('keydown', (e) => {
     switch(e.key) {
@@ -27,7 +31,8 @@ window.onload = () => {
   function clearCanvas() {
     ctx.clearRect(0,0, canvas.width, canvas.height);
   }
-    
+ 
+  //Class Background
   class Background {
      constructor(source) {
        this.posX = 0;
@@ -47,93 +52,211 @@ window.onload = () => {
   
   const background = new Background('./Images/view-of-football-field-vector_test.jpg'); //https://www.vecteezy.com/vector-art/1265829-view-of-football-field
 
+  // Class Player
     class Player {
-    constructor(source, x, y, w, h){
+      constructor(source, x, y, w, h){
+        this.posX = x;
+        this.posY = y;
+        this.width = w;
+        this.height = h;
+        this.speed = 10;
+      
+        const img = new Image();
+        img.src = source;
+        img.onload = () => {
+          this.img = img;
+          // console.log('teste')
+        // this.draw();
+        }
+      }
+      draw() {
+        ctx.drawImage(this.img, this.posX, this.posY, this.width, this.height);
+        // console.log(this)
+      }
+      top(){
+        return this.posY;
+      }
+      bottom(){
+        return this.posY + this.height
+      }
+      left(){
+        return this.posX;
+      }
+      right(){
+        return this.posX + this.width;
+      }
+      moveLeft(){
+        if(this.posX > 0){
+          this.posX -= this.speed;
+        }
+      }
+      moveRight(){
+        if(this.posX < canvas.width - this.width){
+          this.posX += this.speed;
+        }
+      }
+      moveUp(){
+        if(this.posY > 0){
+          this.posY -= this.speed;
+       }
+      }
+      moveDown(){
+        if(this.posY < canvas.height - this.width){
+          this.posY += this.speed;
+        }
+      }
+      checkCollision(ball){
+        return !(
+          this.top() > ball.bottom() || 
+          this.bottom() < ball.top() || 
+          this.left() > ball.right() || 
+          this.right() < ball.left()
+        );
+      }
+    }
+
+  const player = new Player('./Images/Messi.png', 250, 180, 50, 50);
+  
+  // Class Ball
+  class Ball {
+    constructor(source,x,y){
       this.posX = x;
       this.posY = y;
-      this.width = w;
-      this.height = h;
-      this.speed = 10;
-      
+      this.width = 25;
+      this.height = 25;
+
       const img = new Image();
       img.src = source;
       img.onload = () => {
-        this.img = img;
-        console.log('teste')
-        // this.draw();
+        this.img = img; 
       }
     }
     draw() {
       ctx.drawImage(this.img, this.posX, this.posY, this.width, this.height);
-      // console.log(this)
     }
-    moveLeft(){
-      if(this.posX > 0){
-        this.posX -= this.speed;
-      }
+    top(){
+      return this.posY;
     }
-    moveRight(){
-      if(this.posX < canvas.width - this.width){
-        this.posX += this.speed;
-      }
+    bottom(){
+      return this.posY + this.height
     }
-    moveUp(){
-      if(this.posY > 0){
-        this.posY -= this.speed;
-      }
+    left(){
+      return this.posX;
     }
-    moveDown(){
-      if(this.posY < canvas.height - this.width){
-        this.posY += this.speed;
-      }
+    right(){
+      return this.posX + this.width;
     }
   }
 
-  const player = new Player('./Images/Messi.png', 250, 180, 50, 50);
+  
+  let ball1 = new Ball('./Images/soccer-ball.png', 25, 35)
+  let ball2 = new Ball('./Images/soccer-ball.png', 85, 340)
+  let ball3 = new Ball('./Images/soccer-ball.png', 500, 340)
+  let ball4 = new Ball('./Images/soccer-ball.png', 438, 35)
+  let ball5 = new Ball('./Images/soccer-ball.png', 250, 300)
 
+  let balls = [
+    ball1,
+    ball2,
+    ball3,
+    ball4,
+    ball5
+  ]
+
+  function updateBalls() {
+   for(let i = 0; i < balls.length; i+=1){
+     balls[i].draw();
+   } 
+  }
+
+  let score = 0;
+  
+  function Collision(){
+    console.log("collision");
+    for (let i = 0; i < balls.length; i+=1){
+      if(player.checkCollision(balls[i])){
+        score += 1;
+        balls.splice(i,1);
+        console.log("ball");      
+      }
+    }
+  }
+  
+  function showScore(){
+    scoreElement.innerHTML = score;
+  }
+
+  function stopGame() {
+    if(counter === 0){
+      console.log('STOP');
+      cancelAnimationFrame(updateCanvas);
+    }
+  }
+  
+  function gameOver(){
+    if(score >= 4){
+      ctx.fillStyle = "red";
+      ctx.fillText("VICTORY !!", 250, 180);
+    } else {
+      ctx.fillStyle = "black";
+      ctx.fillText("IT'S OVER", 250, 180);
+    }
+  }
+ 
  function updateCanvas(){
   console.log(updateCanvas);
+  showScore();
   clearCanvas();
   background.draw();
-  player.draw();
   walls.forEach((wall) => {wall.draw()})
-  requestAnimationFrame(updateCanvas);
- }
+  player.draw();
+  updateBalls();
+  Collision();
+  if(showTime() > 0){
+    requestAnimationFrame(updateCanvas);
+  } else {
+    stopGame();
+    gameOver();
+  }
+  }
 
   let maze = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
-    [1,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
   ];
   console.log(maze[0].length)
+  let w = 10;
+  let h = 20;
+  
   class Wall {
     constructor(x,y){
       this.posX = x;
       this.posY = y;
-      this.width = 10;
-      this.height = 20;
+      this.width = w;
+      this.height = h;
     }
     draw() {
-      ctx.fillStyle = "red";
+      ctx.fillStyle = "green";
       ctx.fillRect(this.posX, this.posY, this.width, this.height);
-    }    
+    }  
   }
   
   const walls = [];
@@ -141,78 +264,88 @@ window.onload = () => {
   for(let i = 0; i < maze.length; i+=1){ //linha
     for(let j = 0; j < maze[i].length; j+=1){ //coluna
       if(maze[i][j] === 1){
-        walls.push(new Wall(j*10, i*20))
+        walls.push(new Wall(j*w, i*h))
       }      
     }
   }
   
+  let counter = 15
+  let intervalId = setInterval(() => {
+    counter -=1
+    if(counter === 0) clearInterval(intervalId)
+  },1000)
+  
+  function showTime(){
+    return timer.innerHTML = counter;
+  }
+
 
   // function createWall(){
   //   const min
   // }
 
 
-//  setTimeout(updateCanvas, 2000); 
+  //  setTimeout(updateCanvas, 2000); 
 
-// const playerImg = new Image();
-// playerImg.src = 'Images/messi_test.png'
-// ctx.drawImage(playerImg, 50, 50, 200, 200);
-// document.body.appendChild(playerImg);
-// console.log(playerImg)
+  // const playerImg = new Image();
+  // playerImg.src = 'Images/messi_test.png'
+  // ctx.drawImage(playerImg, 50, 50, 200, 200);
+  // document.body.appendChild(playerImg);
+  // console.log(playerImg)
 
-//  updateCanvas();
+  //  updateCanvas();
   
-  
+      
+  // class Timer {
+  //   constructor() {
+  //     this.currentTime = 0;
+  //     this.intervalId = null;
+  //   }
     
-    // class Timer {
-    //     constructor() {
-    //       this.currentTime = 0;
-    //       this.intervalId = null;
-    //     }
-      
-    //     start(callback) {
-    //       this.intervalId = setInterval(() => {
-    //         this.currentTime += 1
-    //       },1000)
-    //     }
-      
-    //     getMinutes() {
-    //       return Math.floor(this.currentTime/60);
-    //     }
-      
-    //     getSeconds() {
-    //       return this.currentTime%60
-    //     }
-      
-    //     computeTwoDigitNumber(value) {
-    //     // let minutes = this.getMinutes();
-    //     // let seconds = this.getSeconds();
-    //     if (value < 10) {
-    //       return '0' + value;
-    //     } else {
-    //       return value.toString();
-    //     }
-    //     }
-    //     // if (seconds < 10) {
-    //     //   seconds = '0' + this.getSeconds();
-    //     // } else if (seconds >= 10) {
-    //     //   seconds = this.getSeconds();
-    //     // }
-    //     // let minutes = (this.getMinutes() < 10 ? '0': '') + this.getMinutes(); 
-    //     // let seconds = (this.getSeconds() < 10 ? '0': '') + this.getSeconds();
-    //     // value = minutes || seconds
-    //     stop() {
-    //       clearInterval(this.intervalId)
-    //     }
-      
-    //     reset() {
-    //       this.currentTime = 0;
-    //     }
-      
-    //     split() {
-    //       let visualSplit = this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds())
-    //       return visualSplit
-    //     }
-    // }
-      
+  //   start() {
+  //     this.intervalId = setInterval(() => {
+  //       this.currentTime += 1
+  //     },1000)
+  //   }
+    
+  //   getMinutes() {
+  //     return Math.floor(this.currentTime/60);
+  //   }
+    
+  //   getSeconds() {
+  //     return this.currentTime%60
+  //   }
+    
+  //   computeTwoDigitNumber(value) {
+  //   if (value < 10) {
+  //     return '0' + value;
+  //   } else {
+  //     return value.toString();
+  //   }
+  //   }
+
+  //   stop() {
+  //     clearInterval(this.intervalId)
+  //   }
+    
+  //   reset() {
+  //     this.currentTime = 0;
+  //   }
+    
+  //   split() {
+  //     let visualSplit = this.computeTwoDigitNumber(this.getMinutes()) + ':' + this.computeTwoDigitNumber(this.getSeconds())
+  //     return visualSplit
+  //   }
+  // }
+
+  // const time = new Timer();
+  
+  // function showTime() {
+  //   time.start();
+  //   time.getMinutes();
+  //   time.getSeconds();
+  //   return timer.innerHTML = time.split();
+  // }
+
+  
 }
